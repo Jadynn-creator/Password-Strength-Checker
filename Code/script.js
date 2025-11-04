dc662fed93c797abd78ae1b3e9935ff4f88de9de
Jadyn Ndirangu <jamesjadyn140@gmail.com>
2025-11-04 19:13:52 +0300

    Added more functinality

--- Code/script.js
+++ Code/script.js
@@ -1,20 +1,26 @@
-let password=document.getElementById("password");
-let strengthBar=document.getElementById("power-point");
+document.addEventListener("DOMContentLoaded", function() {
+const password = document.getElementById("password");
+const power = document.getElementById("power-point");
+const strengthText = document.getElementById("strength-text");
 
-password.oninput=function(){
-    let point=0;
-    let value=password.value;
-    let widthPower=["1%","25%","50%","75%","100%"];
-    let colorPower=["red","orange","yellow","blue","green"];
+password.addEventListener("input", function () {
+    let point = 0;
+    const value = password.value;
+    const widthPower = ["1%", "25%", "50%", "75%", "100%"];
+    const colorPower = ["red", "orange", "yellow", "blue", "green"];
+    const textPower = ["Very Weak", "Weak", "Moderate", "Strong", "Very Strong"];
 
-    if(value.length>=8){
-        let arrayTest =[/[0-9]/,/[a-z]/,/[A-Z]/,/[^0-9a-zA-Z]/];
-
-        arrayTest.forEach((item)=>{
-            if(item.test(value)) point++;
+    if (value.length >= 8) {
+        const tests = [/[0-9]/, /[a-z]/, /[A-Z]/, /[^0-9a-zA-Z]/];
+        tests.forEach((regex) => {
+            if (regex.test(value)) point++;
         });
-        }
-    
-    power.style.width=widthPower[point];
-    power.style.backgroundColor=colorPower[point];
-};
+    } else if (value.length > 0) {
+        point = 1; // weak for short passwords
+    }
+
+    power.style.width = widthPower[point];
+    power.style.backgroundColor = colorPower[point];
+    strengthText.textContent = textPower[point];
+});
+});
